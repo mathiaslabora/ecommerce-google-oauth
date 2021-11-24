e36beb681edc0ed7d8ba422c0e537fd07e9db905
mathias labora martirena <mathiaslabora@gmail.com>
2021-11-24 15:37:49 -0300

    v5

--- js/login.js
+++ js/login.js
@@ -31,6 +31,13 @@ function onSignIn(googleUser) {
     buttonSend();
   }
 
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }
+  
 document.addEventListener("DOMContentLoaded", function (e) {
     document.getElementById("enviar").addEventListener("click", buttonSend);
 });

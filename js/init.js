5066632eac87e368c09501f0a00c37fc0a0cb5a8
mathias labora martirena <mathiaslabora@gmail.com>
2021-11-24 15:41:25 -0300

    v6

--- js/init.js
+++ js/init.js
@@ -15,6 +15,14 @@ var hideSpinner = function () {
   document.getElementById("spinner-wrapper").style.display = "none";
 }
 
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
+  });
+}
+
+
 var getJSONData = function (url) {
   var result = {};
   showSpinner();

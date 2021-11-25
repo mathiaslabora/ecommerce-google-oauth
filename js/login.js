3299239761d66226f87178a196f7849f727a3562
mathias labora martirena <mathiaslabora@gmail.com>
2021-11-25 14:12:14 -0300

    v27

--- js/login.js
+++ js/login.js
@@ -1,6 +1,4 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
+"use strict"
 const data = [];
 const userText = document.getElementById('user');
 const passText = document.getElementById('pass');
